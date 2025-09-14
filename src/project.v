d82e8c1f79d8ac67776e137c7c6f45c785e24524
NeXumZ <171620350+NeXumZ@users.noreply.github.com>
2025-09-14 10:32:44 +0200

    Add files via upload
    cp/paste of https://github.com/wallento/tt09-4bit-toycpu/tree/main/src

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_wallento_4bit_toycpu (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,12 +16,25 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  logic we;
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  cpu u_cpu(
+    .clk (ui_in[0]),
+    .rst (ui_in[1]),
+    .scan_clk (ui_in[2]),
+    .scan_en (ui_in[3]),
+    .scan_out (uo_out[5]),
+    .addr (uo_out[3:0]),
+    .data_out (uio_out),
+    .data_in (uio_in),
+    .we (we)
+  );
+
+  assign uio_oe = {8{we & ui_in[4]}};
+  assign uo_out[4] = we;
+
+  wire [7:0] unused;
+  assign unused = { ena, clk, rst_n, ui_in[7:3] };
+  assign uo_out[7:6] = 2'b0;
 
 endmodule
